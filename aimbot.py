8f84d5e994d2edf90d992740bb231153fecdc0f8
Bùi Hồng Quân <125664684+New0verTime@users.noreply.github.com>
2024-10-15 10:48:24 +0700

    Aimbot v0.4
    Sử dụng yolo11n. Nhanh hơn, và hiệu quả tương đương yolov8s!
    Cải tiến thuật toán aimbot. Tăng tỉ lệ headshot, ngắm bắn nhanh hơn

--- aimbot.py
+++ aimbot.py
@@ -3,59 +3,53 @@ import mss
 import numpy as np
 from ultralytics import YOLO
 import mousekey
-from pynput import keyboard
 import pyautogui
-is_q_pressed = False
 
-def on_release(key):
-    global is_q_pressed
-    try:
-        if key.char == 'q' and is_q_pressed == False:
-            is_q_pressed = True
-        elif key.char == 'q' and is_q_pressed == True:
-            is_q_pressed = False
-    except AttributeError:
-        pass
-listener = keyboard.Listener(on_release=on_release)
-listener.start()
 screen_width, screen_height = pyautogui.size()
 monitor = {"top": (int)((screen_height - 640)/2), "left": (int)((screen_width - 640)/2), "width": 640, "height": 640}
-model = YOLO("best2.pt").to('cuda')
-model2 = YOLO("best.pt").to('cuda')
+model = YOLO("best.pt").to('cuda')
 area_threshold = 2500
 with mss.mss() as sct:
     while True:
         screenshot = sct.grab(monitor)
         img = np.array(screenshot)
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
-        if is_q_pressed == False:
-            results = model(img, conf=0.5)
-        else:
-            results = model2(img, conf=0.5)
+        results = model(img, conf=0.5)
         if len(results[0].boxes):
-            print(dir(results[0].boxes[0]))
             boxes_near = []
             boxes_far = []
             current_pos = pyautogui.position()
-            
+            #convert position sang he toa do cua mat phang detect
+            current_posx = current_pos.x - monitor["left"]
+            current_posy = current_pos.y - monitor["top"]
             for box in results[0].boxes:
                 x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
                 area = (x2 - x1) * (y2 - y1)
                 bbox_center_x = int((x1 + x2) / 2)
-                bbox_center_y = int((y1 + y2) / 2)
-                distance = (bbox_center_x - current_pos.x) ** 2 + (bbox_center_y - current_pos.y) ** 2
+                bbox_center_y = int((3*y1 + y2) / 4)
+                distance = (bbox_center_x - current_posx) ** 2 + (bbox_center_y - current_posy) ** 2
                 if area > area_threshold:
                     boxes_near.append((box, distance))
                 else:
                     boxes_far.append((box, distance))
             if boxes_near:
-                selected_box = min(boxes_near, key=lambda x: x[1])[0]  # Box gần nhất
+                s = min(boxes_near, key=lambda x: x[1])
+                selected_box = s[0]  # Box gần nhất
+                d = s[1]
             else:
-                selected_box = min(boxes_far, key=lambda x: x[1])[0]  # Box xa nhất
+                s = min(boxes_far, key=lambda x: x[1])
+                selected_box = s[0]  # Box gần nhất
+                d = s[1]
             # Lấy tọa độ vật thể được chọn
             x1, y1, x2, y2 = selected_box.xyxy[0].cpu().numpy()
-            bbox_center_x = int((x1 + x2) / 2) + monitor["left"]
-            bbox_center_y = int((y1 + y2) / 2) + monitor["top"]
+            bbox_center_x = int((x1 + x2) / 2)
+            bbox_center_y = int((3*y1 + y2) / 4)
+            print(d)
+            if d < 7000:
+                mousekey.move_rel((int)((bbox_center_x - current_posx) / 5.5), (int)((bbox_center_y - current_posy) / 5.5))
+            elif d < 70000:
+                mousekey.move_rel((int)((bbox_center_x - current_posx) / 7), (int)((bbox_center_y - current_posy) / 7))
+            else:
+                mousekey.move_rel((int)((bbox_center_x - current_posx) / 12), (int)((bbox_center_y - current_posy) / 12))
 
-            # Di chuyển chuột
-            mousekey.move_rel((int)((bbox_center_x - current_pos.x) / 6), (int)((bbox_center_y - current_pos.y) / 6))
+                
