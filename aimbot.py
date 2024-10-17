3615e5a51d8703b53d9a7d3099415cb7eda61608
Bùi Hồng Quân <125664684+New0verTime@users.noreply.github.com>
2024-10-17 21:13:54 +0700

    Update aimbot.py
    Code with note

--- aimbot.py
+++ aimbot.py
@@ -5,14 +5,18 @@ from ultralytics import YOLO
 import mousekey
 import pyautogui
 screen_width, screen_height = pyautogui.size()
+# cắt màn hình ở chính giữa, 640x640
 monitor = {"top": (int)((screen_height - 640)/2), "left": (int)((screen_width - 640)/2), "width": 640, "height": 640}
 model = YOLO("best.pt").to('cuda')
 area_threshold = 2500
 with mss.mss() as sct:
     while True:
         results = model(cv2.cvtColor(np.array(sct.grab(monitor)), cv2.COLOR_BGRA2BGR), conf=0.5)[0].boxes
+        # nếu phát hiện địch
         if len(results):
+            # địch gần
             boxes_near = []
+            # địch xa
             boxes_far = []
             current_pos = pyautogui.position()
             for box in results:
@@ -20,8 +24,10 @@ with mss.mss() as sct:
                 dx = (x1 + x2) // 2 - current_pos.x + monitor["left"]
                 dy = (3 * y1 + y2) // 4 - current_pos.y + monitor["top"]
                 distance = dx ** 2 + dy ** 2
+                # nếu diện tích lớn hơn ngưỡng thì gần
                 if (x2 - x1) * (y2 - y1) - area_threshold > 0:
                     boxes_near.append((distance, dx, dy))  # Lưu distance, dx, dy
+                # nếu ko thì xa
                 else:
                     boxes_far.append((distance, dx, dy))  # Lưu distance, dx, dy
             if boxes_near:
@@ -32,7 +38,6 @@ with mss.mss() as sct:
             if d < 7000:
                 mousekey.move_rel((int)(dx // 5.5), (int)(dy // 5.5))
             elif d < 70000:
-                mousekey.move_rel((int)(dx // 7), (int)(dy // 
-                                                        7))
+                mousekey.move_rel((int)(dx // 7), (int)(dy // 7))
             else:
                 mousekey.move_rel((int)(dx // 12), (int)(dy // 12)) #magic number is magic
